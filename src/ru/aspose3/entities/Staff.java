85961e32132a50b05725a5ae3331b3f0ea7e2944
GennadyStrigunov <40313401+GennadyStrigunov@users.noreply.github.com>
2018-06-21 11:43:42 +0300

    First Commit

---
+++ src/ru/aspose3/entities/Staff.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ru.aspose3.entities;
+
+/**
+ * @author Strigunov Gennady
+ * @version 1.0
+ * Basic parameters of staff.
+ */
+public class Staff {
+  // FIO of Staff
+  String fio;
+  // First working Date
+  java.sql.Date entryDate;
+  // Base rate of salary
+  int baseRate;
+}
